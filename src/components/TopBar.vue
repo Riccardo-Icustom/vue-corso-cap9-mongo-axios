c13c174179e5ebf073fdee9a858c5f8a611bdade
Riccardo De Contardi <riccardo.decontardi@icustomitaly.com>
2023-10-10 11:50:59 +0200

    [TEST]

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -4,12 +4,13 @@
     import { storeToRefs } from 'pinia'
     const taskStore = useTaskStore();
     const {loading}  = storeToRefs(taskStore)
+    const VITE_API_URL = import.meta.env.VITE_API_URL 
 </script>
 
 <template>
     <div class="flex flex-col bg-green-300 w-full mx-auto justify-center items-center rounded-md">
         <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="45" height="45" />
-        <p class="text-xl text-center my-4">Pinia store manager</p>
+        <p class="text-xl text-center my-4">Pinia store manager from {{ VITE_API_URL }}</p>
         <TaskFormComponent v-if="!loading && $route.name === 'tasks'"/>
     </div>
 </template>
